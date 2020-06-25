fe2eb2dafb360abbeb2ea4c4fd0e7983b1ec66f5
Danny <dannyk0104@gmail.com>
2020-06-25 14:45:00 +0900

    init project

---
+++ BackEnd/src/main/java/com/danny/serverusermanagement/ServerUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.danny.serverusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerUserManagementApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/danny/serverusermanagement/ServerUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.serverusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
